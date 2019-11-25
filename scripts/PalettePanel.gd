9644a5f355f254c741df62198dc0ce937809506d
Ryan Shappell <rshapp@uw.edu>
2019-11-25 15:47:20 -0800

    Update scripts/PalettePanel.gd
    Co-Authored-By: Aaron Shappell <ashapp@uw.edu>

--- scripts/PalettePanel.gd
+++ scripts/PalettePanel.gd
@@ -2,7 +2,6 @@ extends PanelContainer
 
 var foregroundPicker: ColorPickerButton
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	foregroundPicker = find_parent("Main").find_node("Foreground")
 
