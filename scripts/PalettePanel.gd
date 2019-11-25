57ea1f8f4bc967f930f84445e2ebc8118e2038d4
Ryan Shappell <rshapp@uw.edu>
2019-11-24 23:47:13 -0800

    can add/remove colors using the color picker

---
+++ scripts/PalettePanel.gd
@@ -0,0 +1,36 @@
+extends PanelContainer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_AddColor_pressed():
+	var image = Image.new()
+	var texture = ImageTexture.new()
+	
+	image.create(16, 16, false, Image.FORMAT_RGBA8)
+	
+	image.fill($Palette/Buttons/ColorPickerButton.color)
+	texture.create_from_image(image)
+	
+	$Palette/Colors.add_icon_item(texture)
+
+
+func _on_RemoveColor_pressed():
+	if $Palette/Colors.is_anything_selected():
+		var selected = $Palette/Colors.get_selected_items()[0]
+		$Palette/Colors.remove_item(selected)
+
+
+func _on_Settings_pressed():
+	pass # Replace with function body.
+
+
+func _on_Colors_item_selected(index):
+	var icon = $Palette/Colors.get_item_icon(index).get_data()
+	icon.lock()
+	$Palette/Buttons/ColorPickerButton.color = icon.get_pixel(1, 1)
