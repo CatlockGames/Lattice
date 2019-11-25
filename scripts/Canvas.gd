f51349d6ff464a51cc04062115fc4b8203cc1747
Aaron Shappell <ashapp@uw.edu>
2019-11-25 00:47:26 -0800

    Start on layer system

--- scripts/Canvas.gd
+++ scripts/Canvas.gd
@@ -1,27 +1,56 @@
-extends ViewportContainer
+extends Control
 
-var textureRect: TextureRect
 var layers := []
+var size: Vector2
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	textureRect = $Viewport/TextureRect
-	
-	var image := Image.new()
-	image.create(16, 16, false, Image.FORMAT_RGBA8)
-	#image.set_pixel(10, 10, Color.red)
-	image.fill(Color.red)
-	var texture := ImageTexture.new()
-	texture.create_from_image(image)
-	layers.append([image, texture])
-	#textureRect.texture.draw(Vector2(20, 20), Color(1, 1, 1, 1))
+	size = Vector2(16, 16)
+	add_layer()
+	layers[0].set_pixel(10, 10, Color.white)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta) -> void:
-#	pass
+func _process(delta) -> void:
+	# Update layers textures as needed
+	for layer in layers:
+		layer.update_texture()
 
 func _draw() -> void:
-	# Transparent background image
-	#todo checker pattern texture
-	#draw_texture(layers[0][1], Vector2(50, 50))
-	textureRect.texture = layers[0][1]
+	# Draw transparent background texture
+	# TODO add checker pattern texture
+	# draw_texture(background, Vector2.ZERO)
+	
+	# Draw layers
+	for layer in layers:
+		if layer.visible:
+			draw_texture(layer.texture, Vector2.ZERO)
+
+func add_layer() -> void:
+	var image := Image.new()
+	var texture := ImageTexture.new()
+	image.create(size.x, size.y, false, Image.FORMAT_RGBA8)
+	texture.create_from_image(image, 0)
+	layers.append(Layer.new(image, texture))
+
+class Layer:
+	var image: Image
+	var texture: ImageTexture
+	var visible: bool
+	var changed: bool
+	
+	func _init(image: Image, texture: ImageTexture) -> void:
+		self.image = image
+		self.texture = texture
+		self.visible = true
+		self.changed = false
+	
+	# Sets the given pixel to the given color
+	func set_pixel(x: int, y: int, color: Color) -> void:
+		image.lock()
+		image.set_pixel(x, y, color)
+		image.unlock()
+		changed = true
+	
+	# Updates the layers texture if any changes were made to its image
+	func update_texture() -> void:
+		if changed:
+			texture.set_data(image)
+			changed = false
