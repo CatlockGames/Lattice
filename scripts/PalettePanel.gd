415e2b59082d4593f374dccb3125dc6e309e5cb1
Ryan Shappell <rshapp@uw.edu>
2019-11-25 15:12:52 -0800

    clean up palette script and organize graphics

--- scripts/PalettePanel.gd
+++ scripts/PalettePanel.gd
@@ -6,32 +6,29 @@ var foregroundPicker: ColorPickerButton
 func _ready():
 	foregroundPicker = find_parent("Main").find_node("Foreground")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+# Adds the current foreground color to the palette
 func _on_AddColor_pressed():
-	var image = Image.new()
-	var texture = ImageTexture.new()
+	var image := Image.new()
+	var texture := ImageTexture.new()
 	
 	image.create(16, 16, false, Image.FORMAT_RGBA8)
 	image.fill(foregroundPicker.color)
 	texture.create_from_image(image)
-	
 	$Palette/Colors.add_icon_item(texture)
 
-
+# Removes selected square from the palette
 func _on_RemoveColor_pressed():
 	if $Palette/Colors.is_anything_selected():
 		var selected = $Palette/Colors.get_selected_items()[0]
 		$Palette/Colors.remove_item(selected)
 
-
+# Palette settings
 func _on_Settings_pressed():
 	pass # Replace with function body.
 
-
+# Sets foreground color to selected palette square
 func _on_Colors_item_selected(index):
 	var icon = $Palette/Colors.get_item_icon(index).get_data()
 	icon.lock()
 	foregroundPicker.color = icon.get_pixel(1, 1)
+	icon.unlock()
