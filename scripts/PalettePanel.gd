31e7787869e8cf2fb49715e441af1ad21c61a99d
Ryan Shappell <rshapp@uw.edu>
2019-11-25 15:53:58 -0800

    create reference for color list

--- scripts/PalettePanel.gd
+++ scripts/PalettePanel.gd
@@ -1,8 +1,10 @@
 extends PanelContainer
 
+var colors: ItemList
 var foregroundPicker: ColorPickerButton
 
 func _ready():
+	colors = $Palette/Colors
 	foregroundPicker = find_parent("Main").find_node("Foreground")
 
 # Adds the current foreground color to the palette
@@ -13,13 +15,13 @@ func _on_AddColor_pressed():
 	image.create(16, 16, false, Image.FORMAT_RGBA8)
 	image.fill(foregroundPicker.color)
 	texture.create_from_image(image)
-	$Palette/Colors.add_icon_item(texture)
+	colors.add_icon_item(texture)
 
 # Removes selected square from the palette
 func _on_RemoveColor_pressed():
-	if $Palette/Colors.is_anything_selected():
-		var selected = $Palette/Colors.get_selected_items()[0]
-		$Palette/Colors.remove_item(selected)
+	if colors.is_anything_selected():
+		var selected = colors.get_selected_items()[0]
+		colors.remove_item(selected)
 
 # Palette settings
 func _on_Settings_pressed():
@@ -27,7 +29,7 @@ func _on_Settings_pressed():
 
 # Sets foreground color to selected palette square
 func _on_Colors_item_selected(index):
-	var icon = $Palette/Colors.get_item_icon(index).get_data()
+	var icon = colors.get_item_icon(index).get_data()
 	icon.lock()
 	foregroundPicker.color = icon.get_pixel(0, 0)
 	icon.unlock()
